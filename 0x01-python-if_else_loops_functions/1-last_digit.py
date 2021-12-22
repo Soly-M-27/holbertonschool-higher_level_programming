e80b66077a04322280d3f3359fc4de11c1391049
Soly-M-27 <solymar227@icloud.com>
2021-12-22 15:07:21 +0000

    Fixed last number positive or negative based on random numbers value

--- 0x01-python-if_else_loops_functions/1-last_digit.py
+++ 0x01-python-if_else_loops_functions/1-last_digit.py
@@ -1,9 +1,15 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10000, 10000)
-if number % 10 > 5:
-    print('Last digit of', number, 'is', (number % 10), 'and is greater than 5')
-elif number % 10 == 0:
-    print('Last digit of', number, 'is', (number % 10), 'and is 0')
-elif number % 10 < 6 and not 0:
-    print('Last digit of', number, 'is', (number % 10), 'and is less than 6 and not 0')
+last = number % 10
+if number < 0:
+    last = number % -10
+else:
+    last = number % 10
+
+if last > 5:
+    print('Last digit of',number,'is',last,'and is greater than 5')
+elif last == 0:
+    print('Last digit of',number,'is',last,'and is 0')
+elif last < 6 and not 0:
+    print('Last digit of',number,'is',last,'and is less than 6 and not 0')
