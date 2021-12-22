5afdd89794a60cc7b08f65e4095a3133f9228d8f
Soly-M-27 <solymar227@icloud.com>
2021-12-22 03:12:59 +0000

    Adding python script that prints double digit 0 to 99 and ends with a new line

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -3,4 +3,4 @@ for i in range(0,100):
     if ((i < 10) or (i >= 10)) and (i != 99):
         print("{0:0=2d}".format(i), end=", ")
     elif i == 99:
-        print("{:d}".format(i))
+        print("{0:0=2d}".format(i))
