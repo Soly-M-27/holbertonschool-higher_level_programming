eb556aae8d0d2caa7ba5648a177e7d8a6bb227ad
Soly-M-27 <solymar227@icloud.com>
2021-12-22 23:15:34 +0000

    Fixed order of if elifs for FizzBuzz option

--- 0x01-python-if_else_loops_functions/12-fizzbuzz.py
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def fizzbuzz():
-    for char in range(1,101):
-        if char % 3 == 0:
-            print('Fizz', end=" ")
+    for char in range(1, 101):
+        if char % 3 == 0 and char % 5 == 0:
+            print('FizzBuzz', end=" ")
         elif char % 5 == 0:
             print('Buzz', end=" ")
-        elif char % 3 == 0 and char % 5 == 0:
-            print('FizzBuzz', end=" ")
+        elif char % 3 == 0:
+            print('Fizz', end=" ")
         else:
             print(char, end=" ")
