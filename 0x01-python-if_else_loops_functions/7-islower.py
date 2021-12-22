ce897e52cafed1b74215057f808b868067b66239
Soly-M-27 <solymar227@icloud.com>
2021-12-22 03:50:03 +0000

    Python function that checks if letter being tested is lower case

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def islower(c):
-    for i in range(ord('a', ord('z')+1):
-            if c == i:
+    for i in range(ord('a'), ord('z')+1):
+        if c == i:
             return True
-            else
+        elif c != i:
             return False
