47d336fec6a682fc17411c392ae666cd826776ba
Soly-M-27 <solymar227@icloud.com>
2021-12-26 20:15:05 +0000

    Fixed file to execute when imported

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-argv = sys.argv
-x = len(argv)
-if x == 1:
-    print("{} arguments.".format(x-1))
-elif x == 2:
-    print("{} argument:".format(x-1))
-    print("{}: {}".format(x-1,argv[x-1]))
-elif x >= 1:
-    print("{} arguments:".format(x-1))
-    for i in range(1, x):
-        print("{}: {}".format(i, argv[i]))
+if __name__ == "__main__":
+    argv = sys.argv
+    x = len(argv)
+    if x == 1:
+        print("{} arguments.".format(x-1))
+    elif x == 2:
+        print("{} argument:".format(x-1))
+        print("{}: {}".format(x-1,argv[x-1]))
+    elif x >= 1:
+        print("{} arguments:".format(x-1))
+        for i in range(1, x):
+            print("{}: {}".format(i, argv[i]))
