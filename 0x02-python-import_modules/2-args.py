e4ec58218228a17ec051f7651341af5fc161e66a
Soly-M-27 <solymar227@icloud.com>
2021-12-26 06:29:08 +0000

    Adding python script that takes a list of arguments provided by the command line and user input to print number of arguments and said arguments next to their index of the list

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+import sys
+argv = sys.argv
+x = len(argv)
+if x == 1:
+    print("{} arguments.".format(x-1))
+elif x == 2:
+    print("{} argument:".format(x-1))
+    print("{}: {}".format(x-1,argv[x-1]))
+elif x >= 1:
+    print("{} arguments:".format(x-1))
+    for i in range(1, x):
+        print("{}: {}".format(i, argv[i]))
