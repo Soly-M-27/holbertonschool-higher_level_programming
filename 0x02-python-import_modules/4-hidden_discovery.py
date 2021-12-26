0e5d3f90295173a5890302a6861c964ac54b535d
Soly-M-27 <solymar227@icloud.com>
2021-12-26 20:21:37 +0000

    Fixed file from executing when imported

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,10 +1,11 @@
 #!/usr/bin/python3
 import hidden_4
-str = dir(hidden_4)
-string_to_compare = str[0][0]
-len = len(str)
-for i in range(0, len):
-    if str[i][0] == string_to_compare:
-        continue
-    else:
-        print(str[i])
+if __name__ == "__main__":
+    str = dir(hidden_4)
+    string_to_compare = str[0][0]
+    len = len(str)
+    for i in range(0, len):
+        if str[i][0] == string_to_compare:
+            continue
+        else:
+            print(str[i])
