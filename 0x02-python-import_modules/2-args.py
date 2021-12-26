03b7cb43dc62b3ff777c61c8c2a936c2d4afcc4a
Soly-M-27 <solymar227@icloud.com>
2021-12-26 20:16:54 +0000

    Fixed file to execute when imported and pycodestyle error

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
         print("{} arguments.".format(x-1))
     elif x == 2:
         print("{} argument:".format(x-1))
-        print("{}: {}".format(x-1,argv[x-1]))
+        print("{}: {}".format(x-1, argv[x-1]))
     elif x >= 1:
         print("{} arguments:".format(x-1))
         for i in range(1, x):
