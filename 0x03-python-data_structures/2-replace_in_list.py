725f4d9a9aba13aa4cd4ca2d01690a60f08b4b4e
Soly-M-27 <solymar227@icloud.com>
2022-01-04 22:47:15 +0000

    Adding script with new return

--- 0x03-python-data_structures/2-replace_in_list.py
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def replace_in_list(my_list, idx, element):
     total_len = len(my_list)
-    if idx < 0:
-        return None
-    if idx > total_len:
+    if idx < 0 or idx > total_len - 1:
         return my_list
     else:
         my_list[idx] = element
