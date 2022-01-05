2b686e6d7eecd85382cd542d233093527366af3e
Soly-M-27 <solymar227@icloud.com>
2022-01-05 21:08:55 +0000

    Adding python script that prints all elements inside a2D array-like matrix

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-    sqr = lambda x: x * x
     new_matrix = []
     for row in matrix:
-        C = list(map(sqr, row))
+        C = list(map(lambda x: x * x, row))
         new_matrix.append(C)
     return new_matrix
-
