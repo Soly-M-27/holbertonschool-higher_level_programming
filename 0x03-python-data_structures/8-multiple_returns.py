229bf9baffd345c0649426775a40247111ae076f
Soly-M-27 <solymar227@icloud.com>
2022-01-05 00:01:50 +0000

    Adding python script that returns a tuple with the length of a sentence and the first letter of said sentence

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -4,7 +4,7 @@ def multiple_returns(sentence):
 
     tuple_space[0] = len(sentence)
     if tuple_space[0] == 0:
-        tuple_space[1] == None
+        tuple_space[1] = None
     else:
         tuple_space[1] = sentence[0:1]
     return tuple(tuple_space)
