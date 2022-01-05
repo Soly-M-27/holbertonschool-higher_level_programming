6029aea95ba90aae3cbe4916e6439acb52a7b616
Soly-M-27 <solymar227@icloud.com>
2022-01-05 00:13:19 +0000

    Fixed if statement and added a top value with first value of list to compare

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,10 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
-    top = 0
-    length = len(my_list)
-    for x in range(length):
-        if my_list[x] > my_list[x + 1]:
-            top = my_list[x]
-        else:
-            top = my_list[x + 1]
-        return top
+    top = my_list[0]
+    for x in my_list:
+        if x > top:
+            top = x
+    return top
