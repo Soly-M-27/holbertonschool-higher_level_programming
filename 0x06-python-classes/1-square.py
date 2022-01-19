ba86ba5e08812c54b1971cd1566bcdf42c4e51f7
Soly-M-27 <solymar227@icloud.com>
2022-01-19 01:36:14 +0000

    Adding documentation

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,4 +1,14 @@
 #!/usr/bin/python3
+""" Module with the class Square """
+
+
 class Square:
+    """ Square Defined """
     def __init__(self, size):
+        """ Docstring of __init___ method
+
+        Args:
+            size (int): size from main to be displayed
+        """
         self._Sqaure__size = size
+        """ int: Docstring *after* attribute, with type specified """
