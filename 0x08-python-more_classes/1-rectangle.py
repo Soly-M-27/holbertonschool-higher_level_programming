dd76e3cba8e0c2e5cc79aafd9c5333f87e5f3d7a
Soly-M-27 <solymar227@icloud.com>
2022-01-24 18:45:49 +0000

    testing

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,56 +1,34 @@
 #!/usr/bin/python3
-""" Module with the class Square """
+""" class Rectangle module"""
 
 
 class Rectangle:
-    """ Rectangle Defined """
+    """ Define a rectangle """
 
     def __init__(self, width=0, height=0):
-        """ Docstring of __init__ method
-
-        Args:
-            width (int): width from main
-            height (int): height from  main
-        """
         self.__height = height
-        """ int: Docstring *after* attribute, with type specified """
         self.__width = width
-        """ int: Docstring *after* attribute, with type specified """
 
-        @property
-        def width(self):
-            """ Docstring of width getter """
-            return self.__width
+    @property
+    def width(self):
+        return self.__width
 
-        @width.setter
-        def width(self, value):
-            """ Docstring of width setter
+    @width.setter
+    def width(self, value):
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        if self.__width < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
 
-            Args:
-                value (int): Contains int from __width attribute
-            """
-            if type(value) != int:
-                raise TypeError("width must be an integer")
-            if self.__width < 0:
-                raise ValueError("width must be >= 0")
-            self.__width = value
-            """ Assigning int to attribute """
+    @property
+    def height(self):
+        return self.__height
 
-        @property
-        def height(self):
-            """ Docstring of heigth getter """
-            return self.__height
-
-        @height.setter
-        def height(self, value):
-            """ Docstring of heigth setter
-
-            Args:
-                value (int): Contains int from __heigth attribute
-            """
-            if type(value) != int:
-                raise TypeError("height must be an integer")
-            if self.__heigth < 0:
-                raise ValueError("height must be >= 0")
-            self.__heigth = value
-            """ Assigning int to attribute """
+    @height.setter
+    def height(self, value):
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        if self.__height < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
