a0c50df7aee5ea85b99f5cbc3ed1c4ef2efa1c44
Soly-M-27 <solymar227@icloud.com>
2022-01-25 03:24:34 +0000

    Use classmethos to return a new Rectangle instance by giving its parameters a new start

--- 0x08-python-more_classes/9-rectangle.py
+++ 0x08-python-more_classes/9-rectangle.py
@@ -6,10 +6,12 @@ class Rectangle:
     """ Define a rectangle """
     number_of_instances = 0
     print_symbol = '#'
+    __height = None
+    __width = None
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
         Rectangle.number_of_instances += 1
 
     @property
@@ -20,7 +22,7 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -32,7 +34,7 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
 
