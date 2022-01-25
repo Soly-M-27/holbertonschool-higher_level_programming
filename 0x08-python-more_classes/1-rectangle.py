bf87f980c449af0f18d1e3ea34649975c675d38b
Soly-M-27 <solymar227@icloud.com>
2022-01-25 00:26:29 +0000

    Added private key instance and fixed edge case attributes

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -4,10 +4,12 @@
 
 class Rectangle:
     """ Define a rectangle """
+    __width = None
+    __height = None
 
     def __init__(self, width=0, height=0):
-        self.__height = height
-        self.__width = width
+        self.height = height
+        self.width = width
 
     @property
     def width(self):
@@ -17,7 +19,7 @@ class Rectangle:
     def width(self, value):
         if type(value) != int:
             raise TypeError("width must be an integer")
-        if self.__width < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
@@ -29,6 +31,6 @@ class Rectangle:
     def height(self, value):
         if type(value) != int:
             raise TypeError("height must be an integer")
-        if self.__height < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
