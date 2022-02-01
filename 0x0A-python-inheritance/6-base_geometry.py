0450042f7b303da8d18ec008d7691c4ca68db104
Soly-M-27 <solymar227@icloud.com>
2022-02-01 02:21:50 +0000

    Print Exception

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Module for BaseGeometry class """
+
+
+class BaseGeometry:
+    """ BaseGeometry defined """
+
+    def area(self):
+        raise Exception("area() is not implemented")
