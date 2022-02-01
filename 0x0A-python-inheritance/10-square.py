e8b334bbbfe937b4f400b2298ef3c3fa85c40576
Soly-M-27 <solymar227@icloud.com>
2022-02-01 04:26:35 +0000

    Area of a square and its values

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,38 +1,20 @@
 #!/usr/bin/python3
 """ Module with Rectangle class inheriting BaseGeometry """
 
-
-class BaseGeometry:
-    """ BaseGeometry defined """
-
-    def area(self):
-        return self.name * self.value
-
-    def __str__(self):
-        print("[Rectangle] {}/{}".format(self.name, self.value))
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
-
-
-class Rectangle(BaseGeometry):
-    """ Rectangle defined """
-    __width = None
-    __height = None
-
-    def __init__(self, width, height):
-        self.__width = width
-        self.__height = height
-        BaseGeometry.integer_validator(self, width, height)
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
-    """ Square defined """
-    __size = None
-
+    """ Comment """
     def __init__(self, size):
         self.__size = size
-        Rectangle.BaseGeometry.integer_validator(self, size, size)
+        BaseGeometry.integer_validator(self, "size", size)
+        BaseGeometry.integer_validator(self, "size", size)
+
+    def area(self):
+        return self.__size * self.__size
+
+    def __str__(self):
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
+
