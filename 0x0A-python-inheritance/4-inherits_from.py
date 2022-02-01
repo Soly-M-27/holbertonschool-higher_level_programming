d9a9120c7dbd736d43a9c28d3b461dded8a05eca
Soly-M-27 <solymar227@icloud.com>
2022-02-01 03:34:17 +0000

    Check if objects are subclasses

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -1,7 +1,11 @@
 #!/usr/bin/python3
+""" Module class """
+
+
 def inherits_from(obj, a_class):
     """ is_kind_of_class defined """
-    if isinstance(obj, a_class) is True:
+
+    if issubclass(type(obj), a_class) and not type(obj) is a_class:
         return True
     else:
         return False
