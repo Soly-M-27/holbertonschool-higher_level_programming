691d8a7462c64c6694f1bd305968781e3b3fbc91
Soly-M-27 <solymar227@icloud.com>
2022-02-01 04:19:17 +0000

    Return area and its values

--- 0x0A-python-inheritance/9-rectangle.py
+++ 0x0A-python-inheritance/9-rectangle.py
@@ -1,29 +1,23 @@
 #!/usr/bin/python3
 """ Module with Rectangle class inheriting BaseGeometry """
 
-
-class BaseGeometry:
-    """ BaseGeometry defined """
-
-    def area(self):
-        return self.name * self.value
-
-    def __str__(self):
-        print("[Rectangle] {}/{}".format(self.name, self.value))
-
-    def integer_validator(self, name, value):
-        if type(value) is not int:
-            raise TypeError("{} must be an integer".format(name))
-        if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+Rectangle = __import__('8-rectangle').Rectangle
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
 
 
 class Rectangle(BaseGeometry):
     """ Rectangle defined """
-    __width = None
-    __height = None
 
     def __init__(self, width, height):
         self.__width = width
         self.__height = height
-        BaseGeometry.integer_validator(self, width, height)
+        BaseGeometry.integer_validator(self, "height", height)
+        BaseGeometry.integer_validator(self, "width", width)
+
+    def area(self):
+        """ Comment """
+        return self.__width * self.__height
+
+    def __str__(self):
+        """ Comment """
+        return "[Rectangle] {}/{}".format(self.__width, self.__height)
