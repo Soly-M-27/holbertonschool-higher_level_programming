6fab8b0f9bc15d51072e14aeb7f589e1a9e6731a
Soly-M-27 <solymar227@icloud.com>
2022-02-02 04:02:34 +0000

    Write a script that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -10,6 +10,6 @@ my_list = []
 try:
     my_list = load_from_json_file(filename)
     my_list.extend(sys.argv[1:])
+    save_to_json_file(my_list, filename)
 except:
     pass
-save_to_json_file(my_list, filename)
