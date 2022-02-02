80c553ac6f4f9566886cf3edba74836736fd2ed3
Soly-M-27 <solymar227@icloud.com>
2022-02-02 01:44:23 +0000

    Writing string to file

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -5,10 +5,6 @@
 def write_file(filename="", text=""):
     """ write_file defined """
 
-    text_file = open(filename)
-    if text_file:
-        with open(filename, "w") as f:
-            return f.write(text)
-    else:
-        with open(filename, "w+") as f:
-            return f.wrire(text)
+    with open(filename, 'w', encoding='utf-8') as file:
+        file.write(text)
+    return len(text)
