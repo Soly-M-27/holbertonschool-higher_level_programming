ebadd68bae45e4ebcdba53bda80af5903afa4b14
Soly-M-27 <solymar227@icloud.com>
2022-02-03 01:04:12 +0000

    pass class attributes to json serialization

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" Module of class """
+import json
+
+
+class Student:
+    """ class Student """
+    first_name = None
+    last_name = None
+    age = None
+
+    def __init__(self, first_name, last_name, age):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ Comment """
+        if attrs is None:
+            return self.__dict__
+        dictionary = {}
+        for x in attrs:
+            try:
+                dictionary[x] = self.__dict__[x]
+            except:
+                pass
+        return dictionary
+
+    def reload_from_json(self, json):
+        if type(json) is dict:
+            for x in json:
+                setattr(self, x, json[x])
