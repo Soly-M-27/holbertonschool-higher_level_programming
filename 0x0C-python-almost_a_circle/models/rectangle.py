468ffbb1f1437351deceb2b937bcc9c65a223595
Soly-M-27 <solymar227@icloud.com>
2022-02-07 22:05:07 +0000

    Added documentation

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -4,7 +4,6 @@
 from models.base import Base
 
 
-
 class Rectangle(Base):
     """ Class Rectangle Defined """
     __width = None
@@ -13,6 +12,14 @@ class Rectangle(Base):
     __y = None
 
     def __init__(self, width, height, x=0, y=0, id=None):
+        """ Docstring of __init__ method
+        Args:
+            width (int): integer representing width of rectangle
+            height (int): integer representing height of rectangle
+            x (int): integer representing a coordinate
+            y (int): integer representing a cooordinate
+            id (int): integer representing an identification number
+        """
         self.x = x
         self.y = y
         super().__init__(id)
@@ -21,10 +28,14 @@ class Rectangle(Base):
 
     @property
     def width(self):
+        """ width getter """
         return self.__width
 
     @width.setter
     def width(self, value):
+        """ width setter
+        Args:
+            value (int): Contains whatever was inside of width """
         if (type(value) != int):
             raise TypeError("width must be an integer")
         if (value <= 0):
@@ -33,10 +44,14 @@ class Rectangle(Base):
 
     @property
     def height(self):
+        """ height getter """
         return self.__height
 
     @height.setter
     def height(self, value):
+        """ height setter
+        Args:
+            value (int): Contains whatever was inside of height """
         if (type(value) != int):
             raise TypeError("height must be an integer")
         if (value <= 0):
@@ -45,10 +60,14 @@ class Rectangle(Base):
 
     @property
     def x(self):
+        """ x coordinate getter """
         return self.__x
 
     @x.setter
     def x(self, value):
+        """ x coordinate setter
+        Args:
+            value (int): Contains whatever was inside of x """
         if (type(value) != int):
             raise TypeError("x must be an integer")
         if (value < 0):
@@ -57,10 +76,14 @@ class Rectangle(Base):
 
     @property
     def y(self):
+        """ y coordinate getter """
         return self.__y
 
     @y.setter
     def y(self, value):
+        """ y coordinate setter
+        Args:
+            value (int): Contains whatever was inside of y """
         if (type(value) != int):
             raise TypeError("y must be an integer")
         if (value < 0):
@@ -68,9 +91,12 @@ class Rectangle(Base):
         self.__y = value
 
     def area(self):
+        """ Docstring of area of the rectangle """
         return self.__width * self.__height
 
     def display(self):
+        """ Docstring of the display of the rectangle based on
+        width and height """
         string = ""
         if self.__width == 0 or self.__height == 0:
             print("")
@@ -89,9 +115,18 @@ class Rectangle(Base):
         print(string[:-1])
 
     def __str__(self):
-        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id, self.__x, self.__y, self.__width, self.__height)
+        """ Docstring __str__ string """
+        return "[Rectangle] ({}) {}/{} - {}/{}\
+                ".format(self.id, self.__x, self.__y, self.__width, self.__height)
 
     def update(self, *args, **kwargs):
+        """ Docstring update, where attributes and dictionaries are actualized
+        Args:
+            *args (pointer to array): Contains all arguments passed by multiple
+            parameter transfer
+            **kwargs (double pointer to dictionary): If *args does not exist
+            then use this to create a dictionary
+        """
         attributes = ["id", "width", "height", "x", "y"]
         if len(args) > 0:
             count = 0
@@ -103,4 +138,6 @@ class Rectangle(Base):
                 setattr(self, key, values)
 
     def to_dictionary(self):
+        """ Docstring to_dictionary method that returns the
+        dictionary representation of a Rectangle """
         return self.__dict__
