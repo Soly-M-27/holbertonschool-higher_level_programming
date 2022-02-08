cf8718b514c8fdf9c75a0b7964facca78cf0a11a
Soly-M-27 <solymar227@icloud.com>
2022-02-08 00:33:47 +0000

    Added raise error in size.setter

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -26,6 +26,10 @@ class Square(Rectangle):
         Args:
             value (int): Contains whatever width of height
             number was passed """
+        if (type(value) != int):
+            raise TypeError("width must be an integer")
+        if (value <= 0):
+            raise ValueError("width must be > 0")
         self.width = value
         self.height = value
 
