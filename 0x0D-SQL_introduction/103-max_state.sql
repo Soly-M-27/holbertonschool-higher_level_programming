c2f0b41764d7da9c040f303d39fc475084653077
Soly-M-27 <solymar227@icloud.com>
2022-03-09 03:13:35 +0000

    Write a script that displays the max temperature of each state (ordered by State name)

--- 0x0D-SQL_introduction/103-max_state.sql
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -1,6 +1,6 @@
 -- Write a script that displays the max temperature
 -- of each state (ordered by State name)
-SELECT state, value
+SELECT state, value AS max_temp
 FROM temperature
 GROUP BY state
 ORDER BY max(temperature)
