2df0bb390901f745d461c8b7d339c1612a2ed1d6
Soly-M-27 <solymar227@icloud.com>
2022-03-10 19:36:48 +0000

    Changed where to do INNER JOIN for testing

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_genres
-INNER JOIN tv_shows ON tv_shows.title = tv_genres.name
+INNER JOIN tv_show_genres ON tv_shows.title = tv_genres.name
 ORDER BY title
 ORDER BY name ASC;
