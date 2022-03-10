f5e834840523f0e81c337fb14e8f7861f07ee43d
Soly-M-27 <solymar227@icloud.com>
2022-03-10 02:49:18 +0000

    Fixed foreign key declaration

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -3,5 +3,6 @@
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS cities (id INT AUTO_INCREMENT PRIMARY KEY NOT NULL, 
-                                    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES id(states),
+                                    state_id INT NOT NULL,
+                                    FOREIGN KEY(state_id) REFERENCES id(states),
                                     name VARCHAR(256) NOT NULL)
