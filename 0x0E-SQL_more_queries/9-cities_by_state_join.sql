6b16b40ef8bed469fdf3026a24aaae6d3f465b64
Soly-M-27 <solymar227@icloud.com>
2022-03-10 03:26:07 +0000

    Fixed branching errors

--- 0x0E-SQL_more_queries/9-cities_by_state_join.sql
+++ 0x0E-SQL_more_queries/9-cities_by_state_join.sql
@@ -1,2 +1,2 @@
 -- Write a script that lists all cities contained in the database hbtn_0d_usa
-SELECT cities.id cities.name states.name FROM cities ORDER BY cities.id ASC;
+SELECT cities.id cities.name states.name FROM cities INNER JOIN states ON cities.states_id ORDER BY cities.id ASC;
