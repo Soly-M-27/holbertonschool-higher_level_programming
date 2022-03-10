bbff39969bdb4e9b3d15ca048e8c5694b69a49bb
Soly-M-27 <solymar227@icloud.com>
2022-03-10 19:12:23 +0000

    Fixed mispelling of genres

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- Write a script that lists all genres from hbtn_0d_tvshows and displays
 -- the number of shows linked to each
 SELECT tv_genres.name AS "genre", COUNT(tv_show_genres.show_id) AS "number_of_shows"
-FROM tv_genre
-INNER JOIN tv_genre ON tv_genre_.id = tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_genres ON tv_genres_.id = tv_show_genres.genre_id
 GROUP BY "genre"
 ORDER BY "number_of_shows" DESC;
