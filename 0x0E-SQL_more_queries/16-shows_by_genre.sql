a87a803c644b8b40a320ce0a0c0067e9bfa22916
Soly-M-27 <solymar227@icloud.com>
2022-03-10 20:03:18 +0000

    Write a script that lists all shows, and all genres linked to that show, from the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -1,7 +1,8 @@
 -- Write a script that lists all shows, and all genres
 -- linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title AS title, tv_genres.name AS name
-FROM tv_genres
-INNER JOIN tv_show_genres ON tv_shows.title = tv_genres.name
+FROM tv_shows
+RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY title
 ORDER BY name ASC;
