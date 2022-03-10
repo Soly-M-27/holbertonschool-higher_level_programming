2d98addc806f53939a1a63fa52aca5f79b88f431
Soly-M-27 <solymar227@icloud.com>
2022-03-10 02:47:35 +0000

    Fixed foreign key declaration

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -2,6 +2,6 @@
 -- (in the database hbtn_0d_usa) on your MySQL server
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 USE hbtn_0d_usa;
-CREATE TABLE IF NOT EXISTS cities (id INT UNIQUE AUTO_GENERATED PRIMARY KEY NOT NULL, 
-                                    state_id INT FOREIGN KEY REFERENCES id(states) NOT NULL,
+CREATE TABLE IF NOT EXISTS cities (id INT AUTO_GENERATED PRIMARY KEY NOT NULL, 
+                                    state_id INT NOT NULL FOREIGN KEY(state_id) REFERENCES id(states),
                                     name VARCHAR(256) NOT NULL)
