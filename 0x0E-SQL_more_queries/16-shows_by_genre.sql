f6d8979ac0caae6cf7cf19267b1c6f90781b788c
Soly-M-27 <solymar227@icloud.com>
2022-03-10 20:04:01 +0000

    Fixed ORDER BY syntax

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -4,5 +4,4 @@ SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows
 RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
-ORDER BY title
-ORDER BY name ASC;
+ORDER BY title, name ASC;
