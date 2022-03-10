5731fabc8cf04d42d931fdd12c67f7d504fa1eaa
Soly-M-27 <solymar227@icloud.com>
2022-03-10 20:04:31 +0000

    Fixed RIGHT JOIN to RIGHT OUTER JOIN

--- 0x0E-SQL_more_queries/16-shows_by_genre.sql
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -2,6 +2,6 @@
 -- linked to that show, from the database hbtn_0d_tvshows
 SELECT tv_shows.title AS title, tv_genres.name AS name
 FROM tv_shows
-RIGHT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
-RIGHT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+RIGHT OUTER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+RIGHT OUTER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 ORDER BY title, name ASC;
