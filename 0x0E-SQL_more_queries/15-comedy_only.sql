691ada5a76366eb8df3abc957d8d639430f8a92c
Soly-M-27 <solymar227@icloud.com>
2022-03-10 20:00:09 +0000

    Write a script that lists all Comedy shows in the database hbtn_0d_tvshows

--- 0x0E-SQL_more_queries/15-comedy_only.sql
+++ 0x0E-SQL_more_queries/15-comedy_only.sql
@@ -5,4 +5,4 @@ FROM tv_shows
 INNER JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
 INNER JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
 WHERE tv_genres.name = "Comedy"
-ORDER BY tv_show.title;
+ORDER BY tv_show.title ASC;
