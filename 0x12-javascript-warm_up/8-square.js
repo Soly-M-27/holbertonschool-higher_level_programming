9f60d53c0e0bdca18828c64de8a658f21bde7a2b
Soly-M-27 <solymar227@icloud.com>
2022-04-04 23:17:21 +0000

    Fixed newline for when argument is negative

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,17 +1,21 @@
 #!/usr/bin/node
 let string = '';
 if (process.argv.length <= 2) {
-  console.log('Missing size');
+	console.log('Missing size');
 } else {
-  for (let x = 0; x < parseInt(process.argv[2]); x++) {
-    for (let z = 0; z < parseInt(process.argv[2]); z++) {
-      string += 'X';
-    }
-    if (x === parseInt(process.argv[2]) - 1) {
-      string += '';
-    } else {
-      string += '\n';
-    }
-  }
-  console.log(string);
+	if (process.argv[2] < 0) {
+		return;
+	} else {
+		for (let x = 0; x < parseInt(process.argv[2]); x++) {
+			for (let z = 0; z < parseInt(process.argv[2]); z++) {
+				string += 'X';
+			}
+			if (x === parseInt(process.argv[2]) - 1) {
+				string += '';
+			} else {
+				string += '\n';
+			}
+		}
+	}
+	console.log(string);
 }
