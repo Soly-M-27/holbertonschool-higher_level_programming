51bf3b503e9f088c03d19a596f74f810644640ee
Soly-M-27 <solymar227@icloud.com>
2022-04-04 23:48:35 +0000

    Fixed semi

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -2,9 +2,9 @@
 const size = process.argv[2];
 
 if (parseInt(size)) {
-	for (let a = 0; a < size; a++) {
-		console.log('X'.repeat(size));
-	}
+  for (let a = 0; a < size; a++) {
+    console.log('X'.repeat(size));
+  }
 } else {
-	console.log('Missing size');
+  console.log('Missing size');
 }
