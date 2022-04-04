94c1d3a5c17fd305c021bcd72f0172f116ff6e45
Soly-M-27 <solymar227@icloud.com>
2022-04-04 23:48:10 +0000

    Print a square

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,23 +1,10 @@
 #!/usr/bin/node
-let string = '';
-if (process.argv.length <= 2) {
-	console.log('Missing size');
-} else {
-	if (process.argv[2] < 0) {
-		return;
-	} else if (!parseInt(process.argv[2])) {
-		return;
-	} else {
-		for (let x = 0; x < parseInt(process.argv[2]); x++) {
-			for (let z = 0; z < parseInt(process.argv[2]); z++) {
-				string += 'X';
-			}
-			if (x === parseInt(process.argv[2]) - 1) {
-				string += '';
-			} else {
-				string += '\n';
-			}
-		}
-		console.log(string);
+const size = process.argv[2];
+
+if (parseInt(size)) {
+	for (let a = 0; a < size; a++) {
+		console.log('X'.repeat(size));
 	}
+} else {
+	console.log('Missing size');
 }
