ae42eaa8c61f50ae980d0ae66a8109e4c3463aa7
Soly-M-27 <solymar227@icloud.com>
2022-04-06 01:15:47 +0000

    Trying less hardcoded attempt

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,15 +6,16 @@ module.exports = class Square extends Rectangle {
     super(size, size);
   }
 
-  charPrint (c) {
-    if (c === undefined) {
-      for (let x = 0; x < this.width; x++) {
-        console.log('X'.repeat(this.height));
-      }
-    } else if (c === 'C') {
-      for (let x = 0; x < this.width; x++) {
-        console.log('C'.repeat(this.height));
-      }
-    }
-  }
+	charPrint(c) {
+		if (c === undefined) {
+			c = 'X';
+			for (let x = 0; x < this.width; x++) {
+				console.log(c.repeat(this.height));
+			}
+		} else if (c === 'C') {
+			for (let x = 0; x < this.width; x++) {
+				console.log(c.repeat(this.height));
+			}
+		}
+	}
 };
