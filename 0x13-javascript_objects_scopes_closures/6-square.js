fd7e27fa150144ddc385156a1a4a92d56f102a7f
Soly-M-27 <solymar227@icloud.com>
2022-04-06 01:16:55 +0000

    Fixed semi

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -6,16 +6,16 @@ module.exports = class Square extends Rectangle {
     super(size, size);
   }
 
-	charPrint(c) {
-		if (c === undefined) {
-			c = 'X';
-			for (let x = 0; x < this.width; x++) {
-				console.log(c.repeat(this.height));
-			}
-		} else {
-			for (let x = 0; x < this.width; x++) {
-				console.log(c.repeat(this.height));
-			}
-		}
-	}
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+      for (let x = 0; x < this.width; x++) {
+        console.log(c.repeat(this.height));
+      }
+    } else {
+      for (let x = 0; x < this.width; x++) {
+        console.log(c.repeat(this.height));
+      }
+    }
+  }
 };
