68286c37e773a56b5b1af8f56e5540bcb0fc7a06
Soly-M-27 <solymar227@icloud.com>
2022-04-09 04:28:48 +0000

    script that prints the State object with the name passed as argument from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+''' Script that prints the first State object from the database '''
+
+from sys import argv
+from sqlalchemy import create_engine, text
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+Sesh = sessionmaker(bind=engine)
+sesh = Sesh()
+rec = sesh.query(State.id, State.name).from_statement(text(
+    "SELECT id, name FROM states WHERE name=:input_name"
+    ).bindparams(input_name=argv[4])).all()
+if rec == []:
+    print("Not found")
+else:
+    for id, name in rec:
+        print("{:d}".format(id))
+sesh.close()
