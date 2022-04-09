764a124e31b047d87f429374fab674ffa22637b8
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:24:41 +0000

    Fixed indentation issues causing logical errors

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,12 +8,12 @@ from sqlalchemy.orm import sessionmaker
 
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-             argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Sesh = sessionmaker(bind=engine)
-sesh = Sesh()
-s = '%a%'
-rec = sesh.query(State).filter(State.name.like(s)).order_by(State.id)
-for st in rec:
-    print("{}: {}".format(st.id, st.name))
-sesh.close()
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Sesh = sessionmaker(bind=engine)
+    sesh = Sesh()
+    s = '%a%'
+    rec = sesh.query(State).filter(State.name.like(s)).order_by(State.id)
+    for st in rec:
+        print("{}: {}".format(st.id, st.name))
+    sesh.close()
