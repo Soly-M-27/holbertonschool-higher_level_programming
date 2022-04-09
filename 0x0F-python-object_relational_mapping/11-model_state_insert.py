e4f68cac83c504192c506da070d838014ab61a09
Soly-M-27 <solymar227@icloud.com>
2022-04-09 04:29:10 +0000

    script that adds the State object “Louisiana” to the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/11-model_state_insert.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Script that prints the first State object from the database '''
+
+from sys import argv
+from sqlalchemy import create_engine, text
+from model_state import State, Base
+from sqlalchemy.orm import sessionmaker
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+             argv[1], argv[2], argv[3]), pool_pre_ping=True)
+Base.metadata.create_all(engine)
+Sesh = sessionmaker(bind=engine)
+sesh = Sesh()
+NewSt = State(name="Louisiana")
+sesh.add(NewSt)
+sesh.commit()
+print(NewSt.id)
+sesh.close()
