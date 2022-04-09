ecb373e9f195294fe993e11985864712fdbf3e39
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:10:19 +0000

    Added comma after argv[4]

--- 0x0F-python-object_relational_mapping/5-filter_cities.py
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -10,7 +10,7 @@ if __name__ == "__main__":
     cur = db.cursor()
     cur.execute("SELECT cities.name FROM cities JOIN states\
                 ON cities.state_id=states.id AND states.name=%s\
-                ORDER BY cities.id ASC", (argv[4]))
+                ORDER BY cities.id ASC", (argv[4],))
     rec = cur.fetchall()
     print(", ".join(x[0] for x in rec))
     cur.close()
