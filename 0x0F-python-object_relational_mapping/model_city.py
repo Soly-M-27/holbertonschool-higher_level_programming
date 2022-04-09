50d908eec5bb744d8c506042bfd161a59435ed96
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:43:43 +0000

    Imported ForeignKey and made id unique

--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,15 +1,15 @@
 #!/usr/bin/python3
 ''' Module defining class City '''
 
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
 from model_state import State
 
 Base = declarative_base()
 
 class City(Base):
-    ''' Class City '''
+    ''' Class City which inherits from State'''
     __tablename__ = "cities"
-    id = Column(Integer, primary_key=True, nullable=False)
+    id = Column(Integer, unique=True, primary_key=True, nullable=False)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
