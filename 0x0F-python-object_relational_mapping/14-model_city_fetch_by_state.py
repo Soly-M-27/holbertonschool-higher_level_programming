14fef3c30cbc436bb482818e5b79bd674ec658c8
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:37:19 +0000

    prints all City objects from the database hbtn_0e_14_usa

---
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+''' Prints all City objects from the database '''
+
+import sqlalchemy
+from sys import argv
+from sqlalchemy import create_engine, orm
+from model_state import Base, State
+from model_city import City
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(*argv[1:4]), pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    session = orm.sessionmaker(bind=engine)()
+    ct = (session.query(State.name, City.id, City.name).filter(
+         City.state_id == State.id).order_by(City.id))
+    for city in ct:
+        print("{}: ({}) {}".format(*city))
+    session.close()
