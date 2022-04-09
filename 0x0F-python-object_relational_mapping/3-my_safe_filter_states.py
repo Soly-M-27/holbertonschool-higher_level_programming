3f1cae78ebc4af2329c5771f31d7a932f498daba
Soly-M-27 <solymar227@icloud.com>
2022-04-09 04:26:11 +0000

    Safer method to take argument and display all values in the states table that matches the argument

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+''' Script to be safe from SQL injections '''
+
+import MySQLdb
+from sys import argv
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
+                         passwd=argv[2], db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
+                (argv[4]))
+    rec = cur.fetchall()
+    for x in rec:
+        print(x)
+    cur.close()
+    db.close()
