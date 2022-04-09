bc947b5b09b68ece501ac3be779d5510ad5591d8
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:17:36 +0000

    Added condition for file to only be executed on main file

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -7,10 +7,11 @@ from sqlalchemy import create_engine, text
 from model_state import State, Base
 from sqlalchemy.orm import sessionmaker
 
-engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
-    argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Sesh = sessionmaker(bind=engine)
-sesh = Sesh()
-rec = sesh.query(State).order_by(State.id)
-for x in rec:
-    print("{}: {}".format(x.id, x.name))
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
+        argv[1], argv[2], argv[3]), pool_pre_ping=True)
+    Sesh = sessionmaker(bind=engine)
+    sesh = Sesh()
+    rec = sesh.query(State).order_by(State.id)
+    for x in rec:
+        print("{}: {}".format(x.id, x.name))
