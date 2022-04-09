a8a483269b08c4cb1a17a1468bde3fc52ba7cf59
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:03:18 +0000

    Readded charset, imported from email

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python3
 ''' Script to be safe from SQL injections '''
 
+from email import charset
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
     db = MySQLdb.connect(host="localhost", port=3306, user=argv[1],
-                         passwd=argv[2], db=argv[3])
+                         passwd=argv[2], db=argv[3], charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT * FROM states WHERE name LIKE %s ORDER BY id ASC",
                 (argv[4]))
