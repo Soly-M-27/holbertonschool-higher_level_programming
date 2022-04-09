1c90cf0bb0aacc6fde1089db8e93ed02f1d72b74
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:26:11 +0000

    Fixed indentation errors

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -9,15 +9,15 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
              argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Sesh = sessionmaker(bind=engine)
-sesh = Sesh()
-rec = sesh.query(State.id, State.name).from_statement(text(
-    "SELECT id, name FROM states WHERE name=:input_name"
-    ).bindparams(input_name=argv[4])).all()
-if rec == []:
-    print("Not found")
-else:
-    for id, name in rec:
-        print("{:d}".format(id))
-sesh.close()
+    Base.metadata.create_all(engine)
+    Sesh = sessionmaker(bind=engine)
+    sesh = Sesh()
+    rec = sesh.query(State.id, State.name).from_statement(text(
+        "SELECT id, name FROM states WHERE name=:input_name"
+        ).bindparams(input_name=argv[4])).all()
+    if rec == []:
+        print("Not found")
+    else:
+        for id, name in rec:
+            print("{:d}".format(id))
+    sesh.close()
