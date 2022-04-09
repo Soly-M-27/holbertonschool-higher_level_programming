cba15515f0611317b30869a6c2d75970fc800b5d
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:29:41 +0000

    Fixed indentation errors

--- 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -10,11 +10,11 @@ from sqlalchemy.orm import sessionmaker
 if __name__ == "__main__":
     engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
              argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
-Sesh = sessionmaker(bind=engine)
-sesh = Sesh()
-State = sesh.query(State).from_statement(text(
-    "SELECT * FROM states WHERE id=2")).one()
-State.name = "New Mexico"
-sesh.commit()
-sesh.close()
+    Base.metadata.create_all(engine)
+    Sesh = sessionmaker(bind=engine)
+    sesh = Sesh()
+    State = sesh.query(State).from_statement(text(
+        "SELECT * FROM states WHERE id=2")).one()
+    State.name = "New Mexico"
+    sesh.commit()
+    sesh.close()
