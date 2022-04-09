46428fabe50b24b110474cf0625cb68a5bdb3566
Soly-M-27 <solymar227@icloud.com>
2022-04-09 18:15:56 +0000

    Deleted Base and changed query

--- 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 ''' List all objects from database '''
 
+import sys
 from sys import argv
 from sqlalchemy import create_engine, text
 from model_state import State, Base
@@ -8,14 +9,8 @@ from sqlalchemy.orm import sessionmaker
 
 engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'.format(
     argv[1], argv[2], argv[3]), pool_pre_ping=True)
-Base.metadata.create_all(engine)
 Sesh = sessionmaker(bind=engine)
 sesh = Sesh()
-rec = sesh.query(State.id, State.name).from_statement(text(
-    "SELECT id, name "
-    "FROM states "
-    "ORDER BY states.id ASC "
-    )).all()
-for id, name in rec:
-    print("{:d}: {:s}".format(id, name))
-    sesh.close()
+rec = sesh.query(State).order_by(State.id)
+for x in rec:
+    print("{}: {}".format(x.id, x.name))
