c0914e640495cd8953d09572cc5b5715fb3e1c6d
Soly-M-27 <solymar227@icloud.com>
2022-04-22 20:32:12 +0000

    Can only execute if main

--- 0x11-python-network_1/3-error_code.py
+++ 0x11-python-network_1/3-error_code.py
@@ -5,10 +5,11 @@ import urllib.request
 import urllib.error
 import sys
 
-try:
-    with urllib.request.urlopen(sys.argv[1]) as response:
-        body = response.read()
-    print("{}".format(body.decode('utf-8')))
-except urllib.error.HTTPError:
-    x = urllib.request.get(sys.argv[1])
-    print("Error code: {}".format(x.status_code))
+if __name__ == "__main__":
+    try:
+        with urllib.request.urlopen(sys.argv[1]) as response:
+            body = response.read()
+        print("{}".format(body.decode('utf-8')))
+    except urllib.error.HTTPError:
+        x = urllib.request.get(sys.argv[1])
+        print("Error code: {}".format(x.status_code))
