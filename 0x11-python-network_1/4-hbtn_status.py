b4901f0032ba1e922577fe34fd1479f763424343
Soly-M-27 <solymar227@icloud.com>
2022-04-22 20:49:52 +0000

    No with for this

--- 0x11-python-network_1/4-hbtn_status.py
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -4,7 +4,7 @@
 import requests
 
 if __name__ == "__main__":
-    with requests.get('https://intranet.hbtn.io/status') as res:
-        print("Body response:")
-        print("\t- type: {}".format(type(res.text)))
-        print("\t- content: {}".format(res.text))
+    body = requests.get('https://intranet.hbtn.io/status')
+    print("Body response:")
+    print("\t- type: {}".format(type(body.text)))
+    print("\t- content: {}".format(body.text))
