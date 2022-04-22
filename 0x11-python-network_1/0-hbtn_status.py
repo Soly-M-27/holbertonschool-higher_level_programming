9a66ad2ad97297edc7bb44cfbfb9add6c7ccffaf
Soly-M-27 <solymar227@icloud.com>
2022-04-22 18:45:16 +0000

    Python script that fetches https://intranet.hbtn.io/status

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+# Python script that fetches https://intranet.hbtn.io/status
+import urllib.request
+
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as response:
+    body = response.read()
+    print("Body response:")
+    print("\t- type:", type(body))
+    print("\t- content:", body)
+    print("\t- utf8 content:", body.decode('utf-8'))
