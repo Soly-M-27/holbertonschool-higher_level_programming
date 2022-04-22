fd05d84f3e6015181b45a8694adf33b231df7888
Soly-M-27 <solymar227@icloud.com>
2022-04-22 19:38:15 +0000

    test

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+''' Python script that takes in a URL and an email,
+    sends a POST request to the passed URL with the
+    email as a parameter, and displays the body of the
+    response (decoded in utf-8) '''
+
+import urllib.request
+import urllib.parse
+import sys
+
+if __name__ == "__main__":
+    data = urllib.parse.urlencode({"email": sys.argv[2]}).encode('utf-8')
+
+    with urllib.request.urlopen(sys.argv[1], data) as res:
+        print("{}".format(res.read()))
