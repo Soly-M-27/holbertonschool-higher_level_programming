46f52d1ba38651c0e370abbca622a10d0357e1cf
Soly-M-27 <solymar227@icloud.com>
2022-05-17 22:33:04 +0000

    Writing to a file with JS

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -5,7 +5,7 @@ const fs = require('fs');
 
 const file = process.argv[2];
 
-const content = process.argv[3] + '\n';
+const content = process.argv[3];
 
 fs.writeFile(file, content, 'utf8', function(err) {
   if (err) {
