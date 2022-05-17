011d6aee1bd1d832b243776c2f994132df1f2512
Soly-M-27 <solymar227@icloud.com>
2022-05-17 23:28:29 +0000

    Star Wars API ID

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -3,13 +3,13 @@
  * Star Wars movie where the episode
  * number matches a given integer */
 
-const axios = require('axios');
+const request = require('request');
 
 const id = process.argv[2];
 
 const url = 'https://swapi-api.hbtn.io/api/films/' + id;
 
-axios(url, function (res, err) {
+request(url, function (res, err) {
   if (err) {
     console.log(err);
   } else {
