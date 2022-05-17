0ada44e88b97d8298c5b68f290896c79b724bff2
Soly-M-27 <solymar227@icloud.com>
2022-05-17 23:06:50 +0000

    Script that displays status code

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -6,9 +6,9 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (error, res) {
-	  if (error) {
-		  console.log(error);
-	  } else {
-		  console.log('code: ' + res.statusCode);
-	  }
+  if (error) {
+    console.log(error);
+  } else {
+    console.log('code: ' + res.statusCode);
+  }
 });
