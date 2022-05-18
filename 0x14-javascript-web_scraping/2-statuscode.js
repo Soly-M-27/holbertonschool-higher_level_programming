e13a4e0e4d92d33026d2d8801f662a623b3b5837
Soly-M-27 <solymar227@icloud.com>
2022-05-18 00:13:17 +0000

    Fixed semi and axios var to request change

--- 0x14-javascript-web_scraping/2-statuscode.js
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 /* Script that displays the status code of a GET request */
 
-const axios = require('axios');
+const request = require('request');
 
 const url = process.argv[2];
 
-axios(url, function (error, res) {
+request(url, function (error, res) {
   if (error) {
     console.log(error);
   } else {
