3a3a72344ef39abc296415ec594b18afae1ec371
Soly-M-27 <solymar227@icloud.com>
2022-05-18 00:18:01 +0000

    Testing dif get methods

--- 0x14-javascript-web_scraping/3-starwars_title.js
+++ 0x14-javascript-web_scraping/3-starwars_title.js
@@ -4,13 +4,13 @@
  * number matches a given integer */
 
 const axios = require('axios');
-const id = proces.argv[2];
-const url = "https://swapi-api.hbtn.io/api/films/";
+const id = process.argv[2];
+const url = 'https://swapi-api.hbtn.io/api/films/';
 
-const res = await axios.get(url, {
-	params: {
-		'episode_id': id
-	}
+const res = axios.get(url, {
+  params: {
+    episode_id: id
+  }
 });
 
-res.data.params['episode_id'];
+console.log(res.data.params.episode_id);
