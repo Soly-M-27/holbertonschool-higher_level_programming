5b2e91a1aa9c68cee2593e8cdacbda2f5d04033a
Soly-M-27 <solymar227@icloud.com>
2022-05-18 03:01:50 +0000

    Fixed semistandart

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -6,16 +6,16 @@ const request = require('request');
 const url = process.argv[2];
 
 request(url, function (res, err, body) {
-	if (err) {
-		console.error(err);
-	}
-	const todo = JSON.parse(body);
-	const completed = {};
-	for (const task in todo) {
-		if (task.completed) {
-			const id = task.userId;
-			completed[id] = (completed[id] + 1 || 1);
-		}
-	}
-	console.log(completed);
+  if (err) {
+    console.error(err);
+  }
+  const todo = JSON.parse(body);
+  const completed = {};
+  for (const task in todo) {
+    if (task.completed) {
+      const id = task.userId;
+      completed[id] = (completed[id] + 1 || 1);
+    }
+  }
+  console.log(completed);
 });
