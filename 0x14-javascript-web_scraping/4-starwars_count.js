701285a67177abc24d221d88b2c0b9c0bd71acd4
Soly-M-27 <solymar227@icloud.com>
2022-05-18 02:13:15 +0000

    Count all instances of a characterin all films

---
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -0,0 +1,25 @@
+#!/usr/bin/node
+/* script that prints the title of a
+ * Star Wars movie where the episode
+ * number matches a given integer */
+
+const request = require('request');
+const url = process.argv[2];
+
+request(url, function (err, res, body) {
+  if (res.statusCode === 200) {
+    const movies = JSON.parse(body).results;
+    for (const id in movies) {
+      const moviestars = movies[id].characters;
+      for (const star in moviestars) {
+        if (moviestars[star].includes('18')) {
+          console.count('Wedge Antilles');
+        }
+      }
+    }
+  } else if (err) {
+    console.log(err);
+  } else {
+    console.log('An error occured. Status code: ' + res.statusCode);
+  }
+});
